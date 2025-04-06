9de2ba4780a1f06a17d7b3a844689726efd8f348
Mike Chen <ywchen@ms1.hinet.net>
2025-04-06 10:48:09 +0800

    fix: use environment PORT for Railway

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return '這是我在雲端的第一個 Python 程式！'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # 從環境變數抓 PORT（預設 5000）
+    app.run(host='0.0.0.0', port=port)
