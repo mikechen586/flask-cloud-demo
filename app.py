51a08c5d2b8128871864221e91ede5d076d04ffb
Mike Chen <ywchen@ms1.hinet.net>
2025-04-06 18:33:32 +0800

    feat: 加入 webhook 取得 userId

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+from flask import Flask, request, jsonify
 import os
-from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '這是我在雲端的第一個 Python 程式！'
+    return '期指操作系統 v1'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    body = request.json
+    events = body.get('events', [])
+    for event in events:
+        if event['type'] == 'message':
+            user_id = event['source']['userId']
+            print(f"你收到訊息來自 userId：{user_id}")
+            return jsonify({'status': 'ok', 'userId': user_id})
+    return jsonify({'status': 'no message'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # 從環境變數抓 PORT（預設 5000）
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
